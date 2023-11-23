961cc326f3f169c7da70c9589560a7eb479d9200
Han JiHo <h2j970313@gmail.com>
2023-11-23 19:50:23 +0900

    first commit

---
+++ src/main/java/com/jiho/anniehands/AnniehandsApplication.java
@@ -0,0 +1,13 @@
+package com.jiho.anniehands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnniehandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnniehandsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiho/anniehands/AnniehandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiho.anniehands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnniehandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
