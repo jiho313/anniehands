ef458e3a674f22ec62fd54e72d40a12455f0aafe
Han JiHo <h2j970313@gmail.com>
2023-11-27 22:42:29 +0900

    add: 루트 홈화면 신상품 화면에 표시하기. Spring Data JPA에 사용할 Entity클래스에 @AllArgsConstructor, @Setter 지양하고 @Builder패턴 사용하기

---
+++ src/main/java/com/jiho/anniehands/product/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.jiho.anniehands.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+
+    // 신상품 5개 조회
+    List<Product> findTop5ByIsEnabledOrderByCreatedDateDesc(Boolean isEnabled);
+
+    // 상품 하나 조회
+    Product findByNo(Long no);
+}
