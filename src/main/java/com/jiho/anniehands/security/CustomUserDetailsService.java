a9e39d6ca29676dd2686b7d41efacacc903ddf65
Han JiHo <h2j970313@gmail.com>
2024-01-03 01:00:26 +0900

    feat: 구글 소셜 로그인 기능 구현 및 일반 UserService와 CustomUserDetailsService 메서드 분리

---
+++ src/main/java/com/jiho/anniehands/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.jiho.anniehands.security;
+
+import com.jiho.anniehands.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+        return userRepository.findById(id)
+            .map(CustomUserDetails::create)
+            .orElseThrow(() -> new UsernameNotFoundException(id));
+    }
+}
