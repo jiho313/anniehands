c8e56d32618639c65deee054f39a1c1384bac1bb
Han JiHo <h2j970313@gmail.com>
2024-01-15 01:04:49 +0900

    refactor: 변수명 수정

--- src/main/java/com/jiho/anniehands/product/ProductService.java
+++ src/main/java/com/jiho/anniehands/product/ProductService.java
@@ -14,18 +14,18 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     @Value("${s3.path.products}")
-    private String s3BasePath;
+    private String s3ProductsPath;
 
     // 상품 번호로 상품조회
     public ProductDto getProductByNo(Long no) {
-        ProductDto productDto = ProductDto.createDto(productRepository.findByNo(no), s3BasePath);
+        ProductDto productDto = ProductDto.createDto(productRepository.findByNo(no), s3ProductsPath);
         return productDto;
     }
 
     // 신상품 5개 조회
     public List<ProductDto> getTop5NewProducts() {
         return productRepository.findTop5ByIsEnabledOrderByCreatedDateDesc(true).stream()
-                .map(product -> ProductDto.createDto(product, s3BasePath)).toList();
+                .map(product -> ProductDto.createDto(product, s3ProductsPath)).toList();
     }
 
 
